d2541f591cf160324039a9189eeae76b9db94904
Javanewbiee <kkremerr@gmail.com>
2019-04-10 12:01:03 +0200

    Modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6, wprowadzenie do testowania oprogramowania");
+    public static void main(String[] args) {
+
+        Calculator calculate = new Calculator(5,5);
+        int sumResult = calculate.add();
+        int substractResult = calculate.substract();
+
+        if(sumResult == 10){
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error");
+        }
+        if(substractResult == 0){
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error");
+        }
     }
 }
