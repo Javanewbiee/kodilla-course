ceafea9e667b2adfa536724c77e114620b12eede
Javanewbiee <kkremerr@gmail.com>
2019-04-15 21:53:10 +0200

    Modul 6.5

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculatingStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculatingStatistics.java
@@ -1,23 +1,46 @@
 package com.kodilla.testing.forum.statistics;
 
+
 import java.util.List;
 
 public class CalculatingStatistics {
 
-    private Statistics statistics;
-
-    public CalculatingStatistics(Statistics statistics) {
-        this.statistics = statistics;
-    }
-
-    int numberOfUsers = 0;
-    int numberOfPosts = 0;
-    int numberOfComments = 0;
-    int averageNumberPostUser = 0;
-    int averageNumberCommentUser = 0;
-    int averageNumberCommentPost = 0;
+    int usersNumber;
+    int numberOfPosts;
+    int numberOfComments;
+    double averageNumberPostUser;
+    double averageNumberCommentUser;
+    double averageNumberCommentPost;
 
     public void calculateAdvStatistics(Statistics statistics){
 
+        usersNumber = statistics.usersNames().size();
+        numberOfPosts = statistics.postsCount();
+        numberOfComments = statistics.commentsCount();
+        averageNumberPostUser = numberOfPosts/usersNumber;
+        averageNumberCommentUser = numberOfComments/usersNumber;
+        averageNumberCommentPost = numberOfComments/numberOfPosts;
+
+    }
+    public int getNumberOfUsers(){
+        return usersNumber;
+    }
+    public int getNumberOfPosts(){
+        return numberOfPosts;
+    }
+    public double getAverageNumberPostUser(){
+        return averageNumberPostUser;
+    }
+
+    public int getNumberOfComments() {
+        return numberOfComments;
+    }
+
+    public double getAverageNumberCommentUser() {
+        return averageNumberCommentUser;
+    }
+
+    public double getAverageNumberCommentPost() {
+        return averageNumberCommentPost;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculatingStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculatingStatisticsTestSuite.java
@@ -1,9 +1,6 @@
 package com.kodilla.testing.forum.statistics;
 
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +13,11 @@ public class CalculatingStatisticsTestSuite {
     private static int counter = 0;
 
     @BeforeClass
-    public void beforeClass(){
+    public static void beforeClass(){
         System.out.println("Starting test");
     }
     @AfterClass
-    public void afterClass(){
+    public static void afterClass(){
         System.out.println("Test ended");
     }
     @Before
@@ -28,15 +25,121 @@ public class CalculatingStatisticsTestSuite {
         counter++;
         System.out.println("Test number " + counter);
     }
+
     @Test
-    public void testCalculateAdvStatisticsUsers(){
+    public void testCalculateAdvStatisticsAverageCommentPostMorePostsThenComments(){
         //Given
         Statistics statisticsMock = mock(Statistics.class);
-        CalculatingStatistics stat = new CalculatingStatistics(statisticsMock);
+        CalculatingStatistics stat = new CalculatingStatistics();
         List<String>usersNames = new ArrayList<String>();
+            usersNames.add("a");
+        int numberOfPosts = 10;
+        int numberOfComments = 2;
+        when(statisticsMock.postsCount()).thenReturn(numberOfPosts);
+        when(statisticsMock.commentsCount()).thenReturn(numberOfComments);
         when(statisticsMock.usersNames()).thenReturn(usersNames);
         //When
-
+        stat.calculateAdvStatistics(statisticsMock);
+        double result = stat.getAverageNumberCommentPost();
+        //Then
+        Assert.assertEquals(0.2, result, 0.2);
     }
-
+    @Test
+    public void testCalculateAdvStatisticsAverageCommentPostMoreCommentsThenPosts(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        CalculatingStatistics stat = new CalculatingStatistics();
+        List<String>usersNames = new ArrayList<String>();
+            usersNames.add("a");
+        int numberOfPosts = 2;
+        int numberOFComments = 10;
+        when(statisticsMock.commentsCount()).thenReturn(numberOFComments);
+        when(statisticsMock.postsCount()).thenReturn(numberOfPosts);
+        when(statisticsMock.usersNames()).thenReturn(usersNames);
+        //When
+        stat.calculateAdvStatistics(statisticsMock);
+        double result = stat.getAverageNumberCommentPost();
+        //Then
+        Assert.assertEquals(5, result, 0);
+    }
+    @Test
+    public void testCalculateAdvStatisticsAveragePostUserMoreUsersThenPosts(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        CalculatingStatistics stat = new CalculatingStatistics();
+        List<String>usersNames = new ArrayList<String>();
+        for(int i =0; i<100; i++){
+            usersNames.add("a");
+        }
+        int numberOfPosts = 10;
+        int numberOfComments = 100;
+        when(statisticsMock.postsCount()).thenReturn(numberOfPosts);
+        when(statisticsMock.usersNames()).thenReturn(usersNames);
+        when(statisticsMock.commentsCount()).thenReturn(numberOfComments);
+        //When
+        stat.calculateAdvStatistics(statisticsMock);
+        double result = stat.getAverageNumberPostUser();
+        //Then
+        Assert.assertEquals(0.1, result, 0.1);
+    }
+    @Test
+    public void testCalculateAdvStatisticsAveragePostUserMorePostsThenUsers(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        CalculatingStatistics stat = new CalculatingStatistics();
+        List<String>usersNames = new ArrayList<String>();
+        for(int i =0; i<100; i++){
+            usersNames.add("a");
+        }
+        int numberOfPosts = 1000;
+        int numberOfComments = 100;
+        when(statisticsMock.usersNames()).thenReturn(usersNames);
+        when(statisticsMock.postsCount()).thenReturn(numberOfPosts);
+        when(statisticsMock.commentsCount()).thenReturn(numberOfComments);
+        //When
+        stat.calculateAdvStatistics(statisticsMock);
+        double result = stat.getAverageNumberPostUser();
+        //Then
+        Assert.assertEquals(10, result, 0);
+    }
+    @Test
+    public void testCalculateAdvStatisticsCommentUserMoreUsersThenComments(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        CalculatingStatistics stat = new CalculatingStatistics();
+        List<String>usersNames = new ArrayList<String>();
+        for(int i =0; i<100; i++){
+            usersNames.add("a");
+        }
+        int numberOfComments = 0;
+        int numberOfPosts = 100;
+        when(statisticsMock.usersNames()).thenReturn(usersNames);
+        when(statisticsMock.commentsCount()).thenReturn(numberOfComments);
+        when(statisticsMock.postsCount()).thenReturn(numberOfPosts);
+        //When
+        stat.calculateAdvStatistics(statisticsMock);
+        double result = stat.getAverageNumberCommentUser();
+        //Then
+        Assert.assertEquals(0, result, 0);
+    }
+    @Test
+    public void testCalculateAdvStatisticsCommentUserMoreCommentsThenUsers(){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        CalculatingStatistics stat = new CalculatingStatistics();
+        List<String>usersNames = new ArrayList<String>();
+        for(int i =0; i<100; i++){
+            usersNames.add("a");
+        }
+        int numberOfComments = 1000;
+        int numberOfPosts = 100;
+        when(statisticsMock.usersNames()).thenReturn(usersNames);
+        when(statisticsMock.commentsCount()).thenReturn(numberOfComments);
+        when(statisticsMock.postsCount()).thenReturn(numberOfPosts);
+        //When
+        stat.calculateAdvStatistics(statisticsMock);
+        double result = stat.getAverageNumberCommentUser();
+        //Then
+        Assert.assertEquals(10, result, 0);
+    }
 }
