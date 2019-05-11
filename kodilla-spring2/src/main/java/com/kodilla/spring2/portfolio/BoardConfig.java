653c25444fd317870be6de0004a361305e35db7f
Javanewbiee <kkremerr@gmail.com>
2019-05-11 09:55:56 +0200

    Modul 10.3

---
+++ kodilla-spring2/src/main/java/com/kodilla/spring2/portfolio/BoardConfig.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring2.portfolio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BoardConfig {
+
+    @Bean
+    public TaskList taskListToDo() {
+        return new TaskList();
+    }
+    @Bean
+    public TaskList taskListInProgress() {
+        return new TaskList();
+    }
+    @Bean
+    public TaskList taskListDone() {
+        return new TaskList();
+    }
+    @Bean
+    public Board board() {
+        return new Board(taskListToDo(),taskListInProgress(),taskListDone());
+    }
+}
---
+++ kodilla-spring2/src/test/java/com/kodilla/spring2/portfolio/BoardTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.spring2.portfolio;
+
+import org.junit.Test;
+
+public class BoardTestSuite {
+
+    @Test
+    public void testTaskAdd(){
+        //Given
+        Task taskToDo = new Task("This has to be done!","TODO");
+        Task taskInProgress = new Task("This is still in progress","INPROGRESS");
+        Task taskDone = new Task("This one is finally done!","DONE");
+        //When
+        Board board = taskDone.addTask();
+        Board board1 = taskInProgress.addTask();
+        Board board2 = taskToDo.addTask();
+        //Then
+        System.out.println(board2.getToDoList().getTasks().get(0));
+        System.out.println(board.getDoneList().getTasks().get(0));
+        System.out.println(board1.getInProgressList().getTasks().get(0));
+    }
+}
