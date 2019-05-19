b088124c9d10353164757d984214c104eaf8ba02
Javanewbiee <kkremerr@gmail.com>
2019-05-19 17:18:16 +0200

    Modul 11.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns.strategy.social;
+
+public class User {
+
+    private String name;
+    protected SocialPublisher socialPublisher;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String share() {
+        return socialPublisher.share();
+    }
+    public void sharePost(SocialPublisher socialPublisher) {
+        this.socialPublisher = socialPublisher;
+    }
+}
---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns/singleton/strategy/UserTestSuite.java
@@ -0,0 +1,40 @@
+package com.kodilla.patterns.singleton.strategy;
+
+import com.kodilla.patterns.strategy.social.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+
+    @Test
+    public void testDefaultSharingStrategies() {
+        //Given
+        User user = new ZGeneration("Riv");
+        User user1 = new YGeneration("Hatsuki");
+        User user2 = new Millenials("Staszek");
+        //When
+        String userUses = user.share();
+        System.out.println(userUses);
+        String user1Uses = user1.share();
+        System.out.println(user1Uses);
+        String user2Uses = user2.share();
+        System.out.println(user2Uses);
+        //Then
+        Assert.assertEquals("User is using Twitter", userUses);
+        Assert.assertEquals("User is using FaceBook", user1Uses);
+        Assert.assertEquals("User is using Snapchat", user2Uses);
+    }
+    @Test
+    public void testIndividualSharingStrategy() {
+        //Given
+        User user = new ZGeneration("Riv");
+        //When
+        String userUses = user.share();
+        System.out.println(userUses);
+        user.sharePost(new FacebookPublisher());
+        String userNowUses = user.share();
+        System.out.println("User changed sharing media and now " + userNowUses);
+        //Then
+        Assert.assertEquals("User is using FaceBook", userNowUses);
+    }
+}
